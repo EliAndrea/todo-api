b848deae02ff01ecd2cf50d54cc3654b2a4ad860
Eli Quijada <eliana.quijada.g@gmail.com>
2019-06-16 19:23:00 -0400

    se agrega modelo user y se modifican endpoints

--- api/models.py
+++ api/models.py
@@ -4,12 +4,20 @@ All your application modules and serializers are going to be declared inside thi
 from rest_framework import serializers
 from django.db import models
 
+class User(models.Model):
+    name = models.CharField(max_length=150, primary_key=True)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= User
+        fields = ('user_name')
+
 class Task(models.Model):
-    user_name = models.CharField(max_length=150, default='')
+    user_name = models.ForeignKey('User', on_delete=models.CASCADE)
     label = models.CharField(max_length=150, default='')
     done = models.BooleanField(default=False)
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('user_name', 'label', 'done')
+        fields = ('label', 'done')
--- api/views.py
+++ api/views.py
@@ -1,34 +1,35 @@
+import json
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.models import Task, TaskSerializer
+from api.models import Task, TaskSerializer, User
 
 
 class TaskView(APIView):
     def get(self, request, user_name):
         tasks = Task.objects.filter(user_name=user_name)
         serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, user_name):
-        new_user = Task(user_name=user_name, label="run", done=False)
-        new_user.save()
-        serializer = TaskSerializer(new_user, many=False)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        newUser = User(name=user_name)
+        newUser.save()
+        response = {"result": "ok"}
+        return Response(response, status=status.HTTP_200_OK)
 
-    def put(self,request, user_name):
+    def put(self, request, user_name):
         tasks = Task.objects.filter(user_name=user_name)
         tasks.delete()
-        print(request.data)
-        tasks_list = request.data["list"]
-        for task in tasks_list:
-            new_task = Task(user_name=user_name, label= task["label"], done=False)
-            new_task.save()
-        new_tasks = Task.objects.filter(user_name=user_name)
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        user = User.objects.get(name=user_name)
+        newList = json.loads(request.body)
+        for task in newList:
+            newTask = Task(user_name=user, label= task["label"], done=task["done"])
+            newTask.save()
+        response = {"result": "ok"}
+        return Response(response, status=status.HTTP_200_OK)
 
     def delete(self, request, user_name):
-        task = Task.objects.filter(user_name=user_name)
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        user = User.objects.get(name=user_name)
+        user.delete()
+        response = {"result": "ok"}
+        return Response(response, status=status.HTTP_204_NO_CONTENT)
